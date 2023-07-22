26f0293458c64cafcf506226e2f617193376904d
Alexandre SCHMITT <schmitt.alex.pro@gmail.com>
2023-07-22 22:41:51 +0200

    Frontend : add stripe lib

--- frontend/shop/src/app/app.component.ts
+++ frontend/shop/src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import { StripeService, StripeCardComponent } from 'ngx-stripe';
+import {
+  StripeCardElementOptions,
+  StripeElementsOptions
+} from '@stripe/stripe-js';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss', './app-mobile.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  @ViewChild(StripeCardComponent) card?: StripeCardComponent;
+
   title = 'shop';
+
+  cardOptions: StripeCardElementOptions = {
+    iconStyle: 'solid',
+    style: {
+      base: {
+        iconColor: '#c4f0ff',
+        color: '#fff',
+        fontWeight: 500,
+        fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+        fontSize: '16px',
+        fontSmoothing: 'antialiased',
+        ':-webkit-autofill': {color: '#fce883'},
+        '::placeholder': {color: '#87bbfd'}
+      },
+      invalid: {
+        iconColor: '#ffc7ee',
+        color: '#ffc7ee'
+      }
+    }
+  };
+
+  elementsOptions: StripeElementsOptions = {
+    locale: 'fr',
+  };
+
+  stripeTest?: FormGroup;
+
+  constructor(private fb: FormBuilder, private stripeService: StripeService) {}
+
+  ngOnInit(): void {
+    this.stripeTest = this.fb.group({
+      name: ['', [Validators.required]]
+    });
+  }
+
+  createToken(): void {
+    const name = this.stripeTest!.get('name')!.value;
+    const cardnumber = this.stripeTest!.get('cardnumber')!.value;
+    console.log("cardnumber : ", cardnumber)
+  }
 }
--- frontend/shop/src/app/app.module.ts
+++ frontend/shop/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgxStripeModule} from "ngx-stripe";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxStripeModule.forRoot('***your-stripe-publishable-key***'),
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
